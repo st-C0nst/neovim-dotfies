637960856c481c6dbb709238da377429ec1841d4
Nicholas Costanza <nicholas.costanz@gmail.com>
2026-01-17 13:39:19 -0500

    blink cmp and better lua lsp

---
+++ lua/plugins/lazydev.lua
@@ -0,0 +1,15 @@
+-- This module configures the lua lsp to recognize vim
+return {
+  {
+    "folke/lazydev.nvim",
+    ft = "lua", -- only load on lua files
+    opts = {
+      library = {
+        -- See the configuration section for more details
+        -- Load luvit types when the `vim.uv` word is found
+        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+      },
+    },
+  },
+}
+
