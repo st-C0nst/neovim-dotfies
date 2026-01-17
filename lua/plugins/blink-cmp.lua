9841565fb0917fa75d16063574ee576190e02168
Nicholas Costanza <nicholas.costanz@gmail.com>
2026-01-17 14:14:43 -0500

    added better lazy loading

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -3,6 +3,7 @@ return {
   'saghen/blink.cmp',
   -- optional: provides snippets for the snippet source
   dependencies = { 'rafamadriz/friendly-snippets' },
+  event = {"InsertEnter", "CmdlineEnter"},
 
   -- use a release tag to download pre-built binaries
   version = '1.*',
