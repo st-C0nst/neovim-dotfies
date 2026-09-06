e6ff1922d7293213b2b54fb202b54406484dadbc
Nicholas Costanza <nicholas.costanz@gmail.com>
2026-09-06 16:50:09 -0400

    moved harpoon binds so pinky doesnt hit most important file

--- lua/plugins/harpoon2.lua
+++ lua/plugins/harpoon2.lua
@@ -18,15 +18,15 @@ return {
     vim.keymap.set("n", "hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
     -- First 4 main files
-    vim.keymap.set("n", "ha", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "hs", function() harpoon:list():select(2) end)
-    vim.keymap.set("n", "hd", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "hf", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "hf", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "hd", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "hs", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "ha", function() harpoon:list():select(4) end)
     -- Next 4 files (if needed)
-    vim.keymap.set("n", "hq", function() harpoon:list():select(5) end)
-    vim.keymap.set("n", "hw", function() harpoon:list():select(6) end)
-    vim.keymap.set("n", "he", function() harpoon:list():select(7) end)
-    vim.keymap.set("n", "hr", function() harpoon:list():select(8) end)
+    vim.keymap.set("n", "hr", function() harpoon:list():select(5) end)
+    vim.keymap.set("n", "he", function() harpoon:list():select(6) end)
+    vim.keymap.set("n", "hw", function() harpoon:list():select(7) end)
+    vim.keymap.set("n", "hq", function() harpoon:list():select(8) end)
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
